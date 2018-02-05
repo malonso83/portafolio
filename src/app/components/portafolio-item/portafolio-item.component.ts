5f3eed04ac8885775990bbacc31ef8024615db15
malonso <malonso@sunflowerenergias.es>
2018-02-05 15:12:19 +0100

    Página Portafolioitem

---
+++ src/app/components/portafolio-item/portafolio-item.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portafolio-item',
+  templateUrl: './portafolio-item.component.html'
+})
+export class PortafolioItemComponent {
+
+  constructor() { }
+  
+}
